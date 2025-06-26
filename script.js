b98bb0f2d5cf2fab065335085fef670eccb467d4
Kaden <kadeneustaquio@gmail.com>
2025-06-26 22:29:56 +0400

    Fixed RGB color

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function makeGrid() {
         // box.style.cssText = "width: 64px; height: 64px;";
         box.style.cssText = "width: " + size + "px; height: " + size + "px;";
         box.classList.add("box");
+        box.style.backgroundColor = "white";
         container.appendChild(box); 
     };
 }
@@ -21,13 +22,34 @@ function toggleEventListener() {
         box.addEventListener("mouseover", () => {
             hoverStart(box);
         });
+        box.addEventListener("mouseout", () => {
+            hoverEnd(box);
+        })
     });
 }
 
-function hoverStart(element) {
-    element.setAttribute("background-color", "red;");
-    element.classList.toggle("red");
+function getRandomRGBColor() {
+  // Generate a random number between 0 and 16777215 (0xFFFFFF, the maximum value for a 24-bit color)
+  let randomNumber = Math.floor(Math.random() * 16777216); 
+
+  // Convert the number to a hexadecimal string
+  let hexColor = randomNumber.toString(16);
+
+  // Pad the string with leading zeros if necessary to ensure it's 6 characters long
+  hexColor = hexColor.padStart(6, '0');
+
+  // Prepend '#' to form a valid CSS hex color code
+  return `#${hexColor}`;
 }
+
+function hoverStart(element) {
+    console.log(getRandomRGBColor()); // Outputs a random hex color like "#A3B4C5"
+    element.style.backgroundColor = getRandomRGBColor();
+}
+
+// function hoverEnd(element) {
+//     element.style.backgroundColor = "white";
+// }
 
 const container = document.querySelector(".container"), grid = 1024;
 
